98d06e66dee867d0a580382b3105f98b5111e291
VictorM4tos <108440162+VictorM4tos@users.noreply.github.com>
2022-08-30 13:04:38 -0300

    FAQ
    Modificação

---
+++ FAQ/index.js
@@ -0,0 +1,13 @@
+var acc = document.getElementsByClassName("Pergunta");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  })}
